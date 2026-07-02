a525b7f325c50d1e598eef3fe49d5183c6c57b3c
kayalvizhi <madhukayal88@gmail.com>
2026-07-02 12:47:49 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Step from "./Step";
+
+function App() {
+  const [step, setStep] = useState(1);
+
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    model: "",
+    car_price: "",
+    card_info: "",
+    expiry_date: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const nextStep = () => {
+    setStep(step + 1);
+  };
+
+  const prevStep = () => {
+    setStep(step - 1);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Form Submitted Successfully!");
+  };
+
+  return (
+    <div className="App">
+      <Step
+        step={step}
+        formData={formData}
+        handleChange={handleChange}
+        nextStep={nextStep}
+        prevStep={prevStep}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
+}
+
+export default App;
